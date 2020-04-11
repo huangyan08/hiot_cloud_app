e6921af5a1339febee6440e59c2319acfae95c84
chenmingqing <806595986@qq.com>
2020-04-12 04:15:46 +0800

    添加MVP测试

--- app/src/main/java/com/example/hiot_cloud/MainActivity.java
+++ app/src/main/java/com/example/hiot_cloud/MainActivity.java
@@ -4,13 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.hiot_cloud.test.mvptest.model.User;
+
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("tag", "test");
+
     }
 }
